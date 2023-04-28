03f06a4229b4e74ee129ccd1057b10471697e5be
viniciusteixeiradias <viniciustdias2001@gmail.com>
2023-04-29 01:32:26 +1000

    frontend: add axios

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,20 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+import axios from 'axios';
+
+const result = ref()
+
+const get = async () => {
+  const { data } = await axios.get('http://127.0.0.1:8000/')
+  result.value = data
+}
+</script>
+
 <template>
   App.vue
+  <button @click="get">Get</button>
+
+  <hr>
+  <h1>Response:</h1>
+  {{ result }}
 </template>
